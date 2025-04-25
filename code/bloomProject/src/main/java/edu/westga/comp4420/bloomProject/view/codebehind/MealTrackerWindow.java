112ddecfc0aca580730915ef71d3e0abc46a73c3
Jupiterlebrun <jl00233@my.westga.edu>
2025-04-25 00:05:41 -0400

    Update MealTrackerWindow
    1) Add method to navigate to mealHistoryWindow

--- code/bloomProject/src/main/java/edu/westga/comp4420/bloomProject/view/codebehind/MealTrackerWindow.java
+++ code/bloomProject/src/main/java/edu/westga/comp4420/bloomProject/view/codebehind/MealTrackerWindow.java
@@ -62,6 +62,24 @@ public class MealTrackerWindow {
 			}
 		});
 
+		this.addStatistticsPageButton.setOnAction(event -> {
+			try {
+				FXMLLoader loader = new FXMLLoader();
+				loader.setLocation(Main.class.getResource(Main.MEAL_HISTORY_WINDOW));
+				Parent parent = loader.load();
+				Scene scene = new Scene(parent);
+				Stage addTaskStage = new Stage();
+				addTaskStage.setTitle(Main.MEAL_HISTORY_WINDOW_TITLE);
+				addTaskStage.setScene(scene);
+				addTaskStage.initModality(Modality.APPLICATION_MODAL);
+				addTaskStage.showAndWait();
+			} catch (IOException e) {
+				Alert alert = new Alert(Alert.AlertType.ERROR);
+				alert.setContentText("Unable to launch Add Meal Window");
+				alert.showAndWait();
+			}
+		});
+
 	}
 
 
