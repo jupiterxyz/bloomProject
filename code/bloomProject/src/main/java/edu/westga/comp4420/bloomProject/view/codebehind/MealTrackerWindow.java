190650b80dddc2c5c05882634568283e76cf5b64
Jupiterlebrun <jl00233@my.westga.edu>
2025-04-25 00:13:31 -0400

    Initialized helper methods in MealTrackerWindow
    1) Initialized private helper method  this.addMealPageButtonHelper(); and this.addStatistticsPageButtonHelper();

--- code/bloomProject/src/main/java/edu/westga/comp4420/bloomProject/view/codebehind/MealTrackerWindow.java
+++ code/bloomProject/src/main/java/edu/westga/comp4420/bloomProject/view/codebehind/MealTrackerWindow.java
@@ -44,6 +44,12 @@ public class MealTrackerWindow {
 			((Node) (event.getSource())).getScene().getWindow().hide();
 		});
 
+		this.addMealPageButtonHelper();
+		this.addStatistticsPageButtonHelper();
+
+	}
+
+	private void addMealPageButtonHelper() {
 		this.addMealPageButton.setOnAction(event -> {
 			try {
 				FXMLLoader loader = new FXMLLoader();
@@ -61,27 +67,27 @@ public class MealTrackerWindow {
 				alert.showAndWait();
 			}
 		});
-
-		this.addStatistticsPageButton.setOnAction(event -> {
-			try {
-				FXMLLoader loader = new FXMLLoader();
-				loader.setLocation(Main.class.getResource(Main.MEAL_HISTORY_WINDOW));
-				Parent parent = loader.load();
-				Scene scene = new Scene(parent);
-				Stage addTaskStage = new Stage();
-				addTaskStage.setTitle(Main.MEAL_HISTORY_WINDOW_TITLE);
-				addTaskStage.setScene(scene);
-				addTaskStage.initModality(Modality.APPLICATION_MODAL);
-				addTaskStage.showAndWait();
-			} catch (IOException e) {
-				Alert alert = new Alert(Alert.AlertType.ERROR);
-				alert.setContentText("Unable to launch Add Meal Window");
-				alert.showAndWait();
-			}
-		});
-
 	}
+		private void addStatistticsPageButtonHelper () {
+			this.addStatistticsPageButton.setOnAction(event -> {
+				try {
+					FXMLLoader loader = new FXMLLoader();
+					loader.setLocation(Main.class.getResource(Main.MEAL_HISTORY_WINDOW));
+					Parent parent = loader.load();
+					Scene scene = new Scene(parent);
+					Stage addTaskStage = new Stage();
+					addTaskStage.setTitle(Main.MEAL_HISTORY_WINDOW_TITLE);
+					addTaskStage.setScene(scene);
+					addTaskStage.initModality(Modality.APPLICATION_MODAL);
+					addTaskStage.showAndWait();
+				} catch (IOException e) {
+					Alert alert = new Alert(Alert.AlertType.ERROR);
+					alert.setContentText("Unable to launch Add Meal Window");
+					alert.showAndWait();
+				}
+			});
 
+		}
 
 
 }
